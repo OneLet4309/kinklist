e19b7fb7b33a97078301d17bd358f0a9ae9a86b4
CodeIsLife42 <189020279+CodeIsLife42@users.noreply.github.com>
2024-11-19 21:57:22 -0500

    remove duplicate JS code

--- kinklist.js
+++ kinklist.js
@@ -54,15 +54,7 @@ $(function(){
     LoadList();
 
     $("#listType").change(function() {
-        fileToRead = $("#listType").val() + '.txt';
-        $.get(fileToRead, function(data) {
-            $('#Kinks').text(data);
-            var selection = inputKinks.saveSelection();
-            var kinksText = $('#Kinks').val();
-            kinks = inputKinks.parseKinksText(kinksText);
-            inputKinks.fillInputList();
-        }, 'text');
-
+        LoadList();
     }); 
     
     inputKinks = {
